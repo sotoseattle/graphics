cda1f754bd04f099e8db42c6e516465ba395f449
Javier Soto <sotoseattle@gmail.com>
2016-01-30 11:16:01 -0800

    a solid bar made of two points (parts/body)

--- examples/cloth/duopole.rb
+++ examples/cloth/duopole.rb
@@ -11,9 +11,10 @@ class Particle < Graphics::Body
 
   COUNT = 10
 
-  def initialize m
+  def initialize m, pos
     self.spoke = []
-    super
+    super m
+    self.position = pos
     self.c = :white
     # self.acceleration = model.gravity
   end
@@ -25,9 +26,41 @@ class Particle < Graphics::Body
 
   class View
     def self.draw w, o
-      w.circle o.x, o.y, 1, o.c, :filled
+      w.circle o.x, o.y, 2, o.c, :filled
     end
   end
+end
+
+class Bar
+  attr_accessor :parts
+
+  def initialize p1, p2
+    self.parts = [p1, p2]
+  end
+
+  def vel= v
+    parts.each { |p| p.velocity = v }
+  end
+
+  def interact
+    parts.each(&:interact)
+  end
+
+  def update
+    parts.each(&:update)
+  end
+
+  class View
+    def self.draw w, o
+      cls = o.parts.first.class.const_get :View
+      o.parts.each do |obj|
+        cls.draw w, obj
+      end
+
+      w.line o.parts.first.x, o.parts.first.y, o.parts.last.x, o.parts.last.y, :yellow
+    end
+  end
+
 end
 
 class Nonesense < Graphics::Simulation
@@ -43,15 +76,13 @@ class Nonesense < Graphics::Simulation
 
     # self.model.gravity = V.new_polar(-90, 0.01)
 
-    b0 = Particle.new model
-    b0.position = V[model.w/2, model.h/2]
-    b0.velocity = V[1,1]
-    b0.c = :green
+    b0 = Particle.new model, V[model.w/2, model.h/2]
+    b1 = Particle.new model, V[model.w/2 + 50, model.h/2 + 20]
 
-    b1 = Particle.new model
-    b1.position = V[model.w/2 + 100, model.h/2 + 100]
+    duo = Bar.new b0, b1
+    duo.vel = V[1, 0]
 
-    register_bodies [b0, b1]
+    register_bodies [duo]
 
   end
 
